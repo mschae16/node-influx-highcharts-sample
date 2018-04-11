181cf04470bae23f60a29b68f63b006a33ff9c60
Margo Schaedel <margoschaedel@gmail.com>
2018-04-11 13:10:13 -0600

    Add http request for tidal info and api key to gitignore

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const Influx = require('node-influx');
+const Influx = require('influx');
+const http = require('http');
 const express = require('express');
 const path = require('path');
 const os = require('os');
 const bodyParser = require('body-parser');
+const apiKey = require('./key.js');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +13,18 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', 3000);
+
+const options = {
+  host: 'api.wunderground.com',
+  port: 80,
+  path: `/api/${apiKey}/tide/q/HI/Hanalei.json`,
+};
+
+http.get(options, (res) => {
+  console.log("Got response: " + res.statusCode);
+}).on('error', (e) => {
+  console.log("Got error: " + e.message);
+});
 
 app.get('/', (request, response) => {
   response.send('Hello world!');
