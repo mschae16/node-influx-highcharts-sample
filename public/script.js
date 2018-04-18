854dc7df7e42b3df1432fc2b97f5cf7a258c89c5
Margo Schaedel <margoschaedel@gmail.com>
2018-04-18 15:58:01 -0600

    Refactor and clean up extraneous code

--- public/script.js
+++ public/script.js
@@ -16,55 +16,53 @@ const fetchAllLocations = () => {
             fetchData('hanalei'),
             fetchData('honolulu'),
             fetchData('kahului')
-          ])
-          .then(parsedRes => {
-            if (parsedRes.length) {
-              const mutatedArray = parsedRes.map( arr => {
-                return Object.assign({}, {
-                  name: arr[0].location,
-                  data: arr.map( obj => Object.assign({}, {
-                    x: (moment(obj.time).unix())*1000,
-                    y:obj.height
-                  }))
-                });
-              });
-
-              Highcharts.setOptions({
-                  colors: ['#508991', '#175456', '#09BC8A', '#78CAD2']
-              });
-
-              Highcharts.chart('container', {
-                chart: {
-                  backgroundColor: {
-                      linearGradient: [0, 600, 0, 0],
-                      stops: [
-                        [0, 'rgb(255, 255, 255)'],
-                        [1, 'rgb(161, 210, 206)']
-                      ]
-                  },
-                  type: 'spline'
-                },
-                title: {
-                  text: 'Hawaii Ocean Tides'
-                },
-                xAxis: {
-                  type: 'datetime'
-                },
-                yAxis: {
-                  title: {
-                    text: 'Height (ft)'
-                  }
-                },
-                plotOptions: {
-                  series: {
-                    turboThreshold: 2000,
-                  }
-                },
-                series: mutatedArray
-              });
-            }
-            return;
+         ])
+        .then(parsedRes => {
+          const mutatedArray = parsedRes.map( arr => {
+            return Object.assign({}, {
+              name: arr[0].location,
+              data: arr.map( obj => Object.assign({}, {
+                x: (moment(obj.time).unix())*1000,
+                y:obj.height
+              }))
+            });
           });
+
+          return Highcharts.chart('container', {
+            colors: ['#508991', '#175456', '#09BC8A', '#78CAD2'],
+            chart: {
+              backgroundColor: {
+                  linearGradient: [0, 600, 0, 0],
+                  stops: [
+                    [0, 'rgb(255, 255, 255)'],
+                    [1, 'rgb(161, 210, 206)']
+                  ]
+              },
+              type: 'spline'
+            },
+            title: {
+              text: 'Hawaii Ocean Tides',
+              style: {
+                'color': '#175456',
+              }
+            },
+            xAxis: {
+              type: 'datetime'
+            },
+            yAxis: {
+              title: {
+                text: 'Height (ft)'
+              }
+            },
+            plotOptions: {
+              series: {
+                turboThreshold: 2000,
+              }
+            },
+            series: mutatedArray
+          });
+        })
+        .catch(error => console.log(error));
 };
 
 fetchAllLocations();
